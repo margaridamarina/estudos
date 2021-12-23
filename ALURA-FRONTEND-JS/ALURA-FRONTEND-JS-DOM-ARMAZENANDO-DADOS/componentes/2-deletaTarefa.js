56b8833c9b59154647441b0a63b459a36760b54f
Margarida <margaridammcm@gmail.com>
2021-12-23 13:23:10 -0300

    "CRUD com JavaScript assíncrono"

--- ALURA-FRONTEND-JS/ALURA-FRONTEND-JS-DOM-ARMAZENANDO-DADOS/componentes/2-deletaTarefa.js
+++ ALURA-FRONTEND-JS/ALURA-FRONTEND-JS-DOM-ARMAZENANDO-DADOS/componentes/2-deletaTarefa.js
@@ -1,14 +1,17 @@
 const deletarTarefa = (atualiza, id) => {
-    
+    const index = id /*10*/
     const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas'))/*6*/
-    atualiza()
+    tarefasCadastradas.splice(index, 1) /*9 remover o item do localStorage. O splice vai receber um ponto inicial e até onde quero remover. Quero remover do meu index, que é do id da minha tarefa, e coloco 1 indicando que vou remover a partir dele ele mesmo*/
+    localStorage.setItem('tarefas', JSON.stringify(tarefasCadastradas))/*8 fazemos alguma transformação e devolvemos para o local storage*/
+    atualiza()/*7*/
+    //para colocar itens no localStorage utilizamos setItem, para pegar os itens do localStorage usamos getItem
 }
 
-const BotaoDeleta = () => {
+const BotaoDeleta = (atualiza, id) => {
     const botaoDeleta = document.createElement('button') /*1*/ //Criar uma variavel botaoDeleta como botao
     // botaoDeleta.classList.add('buttons') //Adicionar uma classe buttons ao elemento botaoDeleta
     botaoDeleta.innerText = 'deletar'/*4*/ //Dizer que tudo que tem dentro da variavel botaoDeleta é a palavra concluir
-    botaoDeleta.addEventListener('click', deletarTarefa) /*2*/ //Adicionar um método que fica escutando o evento click e quando clicamos no botão chamamos a funçãoo concluirTarefa (Criar a função já que ela não existe e só então chamá-la)
+    botaoDeleta.addEventListener('click', () => deletarTarefa(atualiza,id)) /*2*/ //Adicionar um método que fica escutando o evento click e quando clicamos no botão chamamos a funçãoo concluirTarefa (Criar a função já que ela não existe e só então chamá-la)
     return botaoDeleta /*3*/ //Retornar função
 }
 
--- ALURA-FRONTEND-JS/ALURA-FRONTEND-JS-DOM-ARMAZENANDO-DADOS/componentes/3-criaTarefa.js
+++ ALURA-FRONTEND-JS/ALURA-FRONTEND-JS-DOM-ARMAZENANDO-DADOS/componentes/3-criaTarefa.js
@@ -11,29 +11,33 @@ export const handleNovoItem = (evento) => {
     //  Temos que transformar tarefas para ele voltar a ser um objeto, porque é o que passamos. Passamos dados que o cliente pega, é um objeto, e transformamos em string. Temos que transformar isso também em objeto. E como fazemos isso? O JSON.stringify converte para string, e ele tem outro método que chama parse que vai transformar em objeto de volta.
     const input = document.querySelector('[data-form-input]')/*4*/ //Passar a primeira referencia do elemento que tem o atributo data-form-input para a variavel input
     const valor = input.value /*5*/ //Pegar o valor do elemento input
-
+    // E aprendemos que localStorage aceita chave valor em formato de string.
     const calendario = document.querySelector('[data-form-date]')/*15*/ 
     const data = moment(calendario.value)/*16*/
+    const horario = data.format('HH:mm')/*26*/
     const dataFormatada = data.format('DD/MM/YYYY')/*17*/
     const concluida = false /*23*/ // estado atual do botão de concluir, ou seja, se o botão estiver false ele não foi apertado, se estiver true ele foi apertado e vamos colocar o estilo do risco.
 
     const dados = /*18*/{ //estratégia de armazenar esses valores dentro de um objeto e passar eles para a outra função.
         valor,
         dataFormatada,
-        concluida
+        horario,/*26*/
+        concluida,
     }
+    
     const tarefasAtualizadas = [...tarefas, dados] /*20*///pegar -tudo (por isso os ...)-  que tem dentro da tarefa + dados novos
-   
-    localStorage.setItem("tarefas", JSON.stringify(tarefasAtualizadas))/*21*///O que quero agora é pegar o que o usuário digitou e guardar no localStorage. Para isso vamos usar um método chamado setItem. Ele espera dois parâmetros, chave e valor. Tanto a chave quanto o valor tem que ser em formato de string, lembrando um objeto JSON. ////o LocalStorage só aceita guardar informações no formato de strings
+    localStorage.setItem('tarefas', JSON.stringify(tarefasAtualizadas))/*21*///O que quero agora é pegar o que o usuário digitou e guardar no localStorage. Para isso vamos usar um método chamado setItem. Ele espera dois parâmetros, chave e valor. Tanto a chave quanto o valor tem que ser em formato de string, lembrando um objeto JSON. ////o LocalStorage só aceita guardar informações no formato de strings
+    // Tivemos que utilizar o objeto JSON, utilizando essa propriedade stringify, que vai converter o objeto em string para ser aceito. Quando formos pegar esses dados de volta, temos que fazer essa transformação novamente, então utilizamos o parse para pegar as strings e voltar ao formato original. 
     input.value = " " /*14*/ //Limpar o valor de input depois de executada a tarefa
+    
     carregaTarefa()/*21*/ //Toda vez que ele for no handleNovoItem preciso chamar carregarTarefa.
 
 }
 
-export const Tarefa = ({valor, dataFormatada, concluida}) => {/*24 só para o concluida*/ /*7*///Aqui era const criarTarefa = (evento) => {//Quem vou passar aqui dentro do criarTarefa? Exatamente, dados. Vou optar por usar object destruction para deixar mais claro quais são os parâmetros que estou passando dentro da tarefa. Faço chaves e valor e dataFormatada.
+export const Tarefa = ({valor, horario, concluida}, id) => {/*24 só para o concluida*/ /*7*///Aqui era const criarTarefa = (evento) => {//Quem vou passar aqui dentro do criarTarefa? Exatamente, dados. Vou optar por usar object destruction para deixar mais claro quais são os parâmetros que estou passando dentro da tarefa. Faço chaves e valor e dataFormatada.
     const tarefa = document.createElement('li')/*7*/ //Criar uma variavel tarefa como list item
     
-    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`/*8 valor*/ /*16 dataFormatada*/ //Criar uma variavel conteudo com uma string que tem uma notacao de um elemento html com o conteudo de valor  com a classe content
+    const conteudo = `<p class="content">${horario} * ${valor}</p>`/*8 valor*/ /*27 horario*/ //Criar uma variavel conteudo com uma string que tem uma notacao de um elemento html com o conteudo de valor  com a classe content
     if(concluida){/*25*/
        tarefa.classList.add('done') /*26*/
     }
@@ -41,7 +45,7 @@ export const Tarefa = ({valor, dataFormatada, concluida}) => {/*24 só para o co
     tarefa.innerHTML = conteudo/*9*/ //Dizer que tudo que tem dentro da variavel tarefa é o conteudo da variavel conteudo
     
     tarefa.appendChild(BotaoConclui(carregaTarefa, id))/*12*/ //Adicionar uma nova child com o retorno da funcao BotaoConclui no final de tarefa
-    tarefa.appendChild(BotaoDeleta())/*13*/ //Adicionar uma nova child com o retorno da funcao BotaoDeleta no final de tarefa
+    tarefa.appendChild(BotaoDeleta(carregaTarefa, id))/*13*/ //Adicionar uma nova child com o retorno da funcao BotaoDeleta no final de tarefa
 
     return tarefa
 }

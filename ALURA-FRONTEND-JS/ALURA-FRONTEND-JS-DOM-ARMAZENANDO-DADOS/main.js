0357c72b0116eaddf9f5a9a0994fa402e9e8c3c8
Margarida <margaridammcm@gmail.com>
2022-02-01 21:07:10 -0300

    Deletando to-do-list repetida

--- ALURA-FRONTEND-JS/ALURA-FRONTEND-JS-DOM-ARMAZENANDO-DADOS/main.js
+++
@@ -1,8 +0,0 @@
-import { handleNovoItem } from "./componentes/3-criaTarefa.js" //entre {} importa só a função /*3*/
-import { carregaTarefa } from "./componentes/4-carregaTarefa.js" /*4*/
-//ponto de execução da aplicação
-const novaTarefa = document.querySelector('[data-form-button]')/*1*/ //Passar a primeira referencia do elemento que tem o atributo data-form-button para a variavel novaTarefa
-novaTarefa.addEventListener('click', handleNovoItem) /*2*/ //Adicionar um método que fica escutando o evento click e quando clicamos no botão chamamos a função criarTarefa (Criar a função já que ela não existe e só então chamá-la)
-
-carregaTarefa() /*5*/ //Depois que ele fizer a parte que ele vai no handleNovoItem, percorre nossa aplicação toda, ele vai carregar as tarefas para mim.
-

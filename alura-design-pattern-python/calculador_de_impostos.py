9d8a829a19b1165049b6d2cdecb59121ab0fc59d
Margarida <margaridammcm@gmail.com>
2022-03-14 22:01:52 -0300

    Iniciando estudos de boas práticas

---
+++ alura-design-pattern-python/calculador_de_impostos.py
@@ -0,0 +1,13 @@
+from impostos import ISS, ICMS
+
+class CalculadorDeImpostos:
+  def realiza_calculo(self, orcamento, imposto):
+    imposto_calculado = imposto.calcula(orcamento)
+    print(imposto_calculado)
+
+if __name__ == '__main__':
+    from orcamento import Orcamento
+    calculador = CalculadorDeImpostos()
+    orcamento = Orcamento(500)
+    calculador.realiza_calculo(orcamento, ISS())
+    calculador.realiza_calculo(orcamento, ICMS())

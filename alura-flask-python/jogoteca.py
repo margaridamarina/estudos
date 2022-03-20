0e9ad1f6a91b4ca8b8535a742eeca906d9bdadc2
Margarida <margaridammcm@gmail.com>
2022-03-20 17:29:58 -0300

    Iniciando estudos com Flask

---
+++ alura-flask-python/jogoteca.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request, redirect, session, flash
+#criando uma variavel onde vamos colocar a aplicacao
+#chamando a funcao flask onde vamos chamar name que faz uma referencia a esse proprio arquivo garantindo que vai rodar a aplicacao
+
+class Jogo: 
+  def __init__(self, nome, categoria, console):
+    self.nome = nome
+    self.categoria = categoria
+    self.console = console
+
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista = [jogo1, jogo2, jogo3]
+
+app = Flask(__name__) 
+app.secret_key = 'alura'
+
+#criando rota com funcao que define o que vai existir nessa rota
+@app.route('/')
+def index():
+  return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/novo')
+def novo():
+  return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+  nome = request.form['nome']
+  categoria = request.form['categoria']
+  console = request.form['console']
+  jogo = Jogo(nome, categoria, console)
+  lista.append(jogo)
+  return redirect('/')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    if 'alohomora' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(request.form['usuario'] + ' logou com sucesso!')
+        return redirect('/')
+    else:
+        flash('Usuário não logado.')
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+  session['usuario_logado'] = None
+  flash('Logout efetuado com sucesso!')
+  return redirect('/')
+#rodando aplicacao
+if __name__ == "__main__":
+  app.run(debug=true)
+# host='0.0.0.0', port=8080
--- alura-python-poo/encapsulamento.py
+++ alura-python-poo/encapsulamento.py
@@ -8,6 +8,13 @@ class Retangulo:
     def obter_area(self):
         return self.__area
 
+    @staticmethod
+    def somaum(x):
+        return x + 1
+
 r = Retangulo(7,6)
 r.area = 7
 print(r.obter_area())
+# print(r.area)
+# print(r.somaum(2))
+print(r.somaum(2))

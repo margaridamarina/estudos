8703f9834fa3603612889fe4b171c3115eda2324
Margarida <margaridammcm@gmail.com>
2022-04-13 07:58:17 -0300

    Alterando status de respostas

--- alura-nodejs-api-rest/api/rotas/fornecedores/index.js
+++ alura-nodejs-api-rest/api/rotas/fornecedores/index.js
@@ -5,6 +5,7 @@ const Fornecedor = require('./fornecedor') //usar classe Fornecedor nas nossas r
 
 roteador.get('/', async (requisicao, resposta) => { //metodo get para obter os dados //como vamos nos comuncicar com um banco de dados com um serviço externo usamos promessa
     const resultados = await TabelaFornecedor.listar() //esperar o metodo terminar de executar
+    resposta.status(200)
     resposta.send( //enviar resposta
         JSON.stringify(resultados) //retornar resposta
     )
@@ -15,10 +16,12 @@ roteador.post('/', async (requisicao, resposta) => { //usar o metodo post para e
     const dadosRecebidos = requisicao.body
     const fornecedor = new Fornecedor(dadosRecebidos)
     await fornecedor.criar() 
+    resposta.status(201)
     resposta.send(
         JSON.stringify(fornecedor)
     )
    } catch (erro) {
+        resposta.status(400)
         resposta.send(
             JSON.stringify({
                 mensagem: erro.message
@@ -33,10 +36,12 @@ roteador.get('/:idFornecedor', async (requisicao, resposta) => { //metodo get pa
         const id = requisicao.params.idFornecedor //a gente consegue acessar dentro dessa funcao do get o id do nosso fornecedor pelos parametros
         const fornecedor = new Fornecedor({id: id}) //instanciar classe do fornecedor, passando como parametro um objeto
         await fornecedor.carregar()
+        resposta.status(200)
         resposta.send(
             JSON.stringify(fornecedor)
         )
     } catch (erro) {
+        resposta.status(404)
         resposta.send(
             JSON.stringify({
                 mensagem: erro.message
@@ -52,8 +57,10 @@ roteador.put('/:idFornecedor', async (requisicao, resposta) => { //metodo put pa
         const dados = Object.assign({}, dadosRecebidos, {id: id}) //juntar variaveis em um objeto so
         const fornecedor = new Fornecedor(dados) //instanciar classe do fornecedor
         await fornecedor.atualizar()
+        resposta.status(204)
         resposta.end() //qd atualizamos algo em uma api rest nao precisamos retornar nenhuma informacao para quem ta construindo a api, apenas informar que teve sucesso
     } catch (erro) {
+        resposta.status(400)
         resposta.send(
             JSON.stringify({
                 mensagem: erro.message
@@ -68,8 +75,10 @@ roteador.delete('/:idFornecedor',async (requisicao, resposta) => {
         const fornecedor = new Fornecedor({id: id}) //instanciar classe do fornecedor, passando como parametro um objeto
         await fornecedor.carregar() //verificar se o fornecedor existe, se nao existir vai emitir um erro
         await fornecedor.remover() //remover fornecedor pela requisicao
+        resposta.status(204)
         resposta.end() //se a requisicao terminar e nao tivermos nenhum erro, foi um sucesso
     } catch (erro) {
+        resposta.status(404)
         resposta.send(
             JSON.stringify({
                 mensagem: erro.message

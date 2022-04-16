befbfa7e4e661d29454c98a362961069bbb8dd7a
Margarida <margaridammcm@gmail.com>
2022-04-16 11:46:15 -0300

    Tratamento de conteudo

--- alura-nodejs-api-rest/api/index.js
+++ alura-nodejs-api-rest/api/index.js
@@ -5,6 +5,7 @@ const config = require('config') //consumir os dados do arquivo de config defaul
 const NaoEncontrado = require('./erros/naoEncontrado')
 const CampoInvalido = require('./erros/campoInvalido')
 const DadosNaoFornecidos = require('./erros/dadosNaoFornecidos')
+const ValorNaoSuportado = require('./erros/valorNaoSuportado')
 
 app.use(bodyParser.json()) //servir como plugin pro nosso app
 
@@ -12,14 +13,20 @@ const roteador = require('./rotas/fornecedores')
 const res = require('express/lib/response')
 app.use('/api/fornecedores', roteador) //declarando primeira requisicao, consumir rota dentro do arquivo da api, passou um grupo de funcoes
 
-app.use((erro, requisicao, resposta, proximoMeader) => {
+app.use((erro, requisicao, resposta, proximoMiddleware) => {
     let status = 500 //erro generico que representa problema na api que nao sabemos o que aconteceu
     if(erro instanceof NaoEncontrado) {
         status = 404
     } 
+    
     if(erro instanceof CampoInvalido || erro instanceof DadosNaoFornecidos) {
         status = 400
     }
+    
+    if(erro instanceof ValorNaoSuportado) {
+        status = 406 //tipo de valor que o cliente pede nao é suportado pela nossa api
+    }
+
     resposta.status(status)
     resposta.send(
         JSON.stringify({
---
+++ alura-nodejs-api-rest/api/serializador.js
@@ -0,0 +1,16 @@
+const ValorNaoSuportado = require('./erros/valorNaoSuportado')
+
+//tratar dados
+class Serializador {
+    json (dados) { //metodo para serializar os dados em json recebendo os dados que queremos transformar
+        return JSON.stringify(dados) //transformar um dado javascript em json
+    }
+    //para saber o tipo de conteudo que o frontend vai aceitar ou esta pedindo > vai em headers na aba de atualizacao (put ou post)> content-type
+    serializar(dados){
+        if (this.contentType === 'application/json') {
+            return this.json(dados)
+        }
+
+        throw new ValorNaoSuportado(this.contentType)
+    }
+}

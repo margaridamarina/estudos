75b848b7d90b80edf9edd959f764cda7121316cb
Margarida <margaridammcm@gmail.com>
2022-04-17 18:21:52 -0300

    Corrigindo erro de digitacao

--- alura-nodejs-api-rest/api/erros/campoInvalido.js
+++ alura-nodejs-api-rest/api/erros/campoInvalido.js
@@ -1,6 +1,6 @@
 class CampoInvalido extends Error {
     constructor(campo) {
-        const mensagem = `O campo '${campo} está inválido!`
+        const mensagem = `O campo ${campo} está inválido!`
         super(mensagem)
         this.name = 'CampoInválido'
         this.idErro = 1
